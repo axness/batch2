1b736a818bb7985153bd9e310ce401fbf272ae8f
nageswararao.ullinda <nageswararao.ullinda@sungardas.com>
2018-03-26 09:16:00 +0530

    28/03/2018

---
+++ arth_int.py
@@ -0,0 +1,12 @@
+a = 5
+b = 10
+print "a+b=",a+b
+print "a-b=",a-b
+print "a*b=",a*b
+print "a/b=",a/float(b)
+print "a//b=",a//b
+print "a%b=",a%b
+print "a**2=",a**2
+print "-4%3=", -4%3
+print "pow(5,2)", pow(5,2)
+print "pow(5,2,10)", pow(5,2,b)
---
+++ print.py
@@ -0,0 +1,9 @@
+print("Hello World")
+print 5,6.5,"python"
+a = 5
+b = 6.5
+c = "python"
+print a,b,c
+d = 15
+e = "class"
+print a,10,d,25
