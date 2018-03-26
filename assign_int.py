c6bfb7efdd3f415d760f09e34983b73a64e45405
nageswararao.ullinda <nageswararao.ullinda@sungardas.com>
2018-03-26 10:15:28 +0530

    26/07/2018

---
+++ assign_int.py
@@ -0,0 +1,15 @@
+#=, +=, -+, *=
+#print a=5
+a = 5
+print "a=5 is ", a
+a += 10
+print a
+a -= 3
+print a
+#print c
+#b += 7
+#print b
+a = 2
+a *= 5
+print a
+
---
+++ log_int.py
@@ -0,0 +1,15 @@
+#logical
+# and or not
+# we will always use logical operators with some condition
+"""print "3 and 0", 3 and 0
+#print "3 and 2", 3 and 2
+print "3 and 5", 3 and 5
+
+if 3 and 5:
+    print "output is true"
+
+if 3 and 0:
+    print "second output is true" """
+
+
+
