5185ea63def1159aa174a3d32ee233dd72d15383
nageswararao.ullinda <nageswararao.ullinda@sungardas.com>
2018-04-26 11:32:13 +0530

    26/04/2018

---
+++ function2.py
@@ -0,0 +1,16 @@
+#types of arguments
+"""
+1. mandatory arguments
+2. default arguments 
+3. variable length arguments
+4. variable length keyword arguments
+"""
+def myfun(a, b):
+    return a+b 
+print myfun(5, 10)
+
+def myfun(a, b):
+    return a+b
+print myfun(3, 5)
+
+def my
---
+++ mod.py
@@ -0,0 +1,6 @@
+#import arth as a
+from arth import *
+
+print addi(5, 6)
+print addi.__doc__
+print subs(4, 8)
